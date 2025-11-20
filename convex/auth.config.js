3eecca9d72129806c8dc6ee998cab4bebaf4115e
cgit6 <109117801+cgit6@users.noreply.github.com>
2025-11-20 14:55:47 +0000

    Refactor auth configuration

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,8 +1,12 @@
-export default {
+// convex/auth.config.ts
+
+const authConfig = {
   providers: [
     {
       domain: "https://novel-chimp-55.clerk.accounts.dev",
       applicationID: "convex",
     },
-  ]
+  ],
 };
+
+export default authConfig;
